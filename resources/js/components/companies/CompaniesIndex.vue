d183262e57620fb9b81d5b7ffc3db18ab6ffbc8b
vim <9wimu9@gmail.com>
2022-05-25 22:06:09 +0530

    In the Composable file resources/js/composable/companies.js we add one more method that calls the Delete API endpoint. Also, we need to return that method at the bottom:

--- resources/js/components/companies/CompaniesIndex.vue
+++ resources/js/components/companies/CompaniesIndex.vue
@@ -19,6 +19,7 @@
                     <span
                         class="text-xs font-medium tracking-wider leading-4 text-left text-gray-500 uppercase">Website</span>
                 </th>
+                <th class="bg-gray-50"></th>
             </tr>
             </thead>
 
@@ -37,6 +38,11 @@
                     <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
                         {{ item.website }}
                     </td>
+                    <td class="px-6 py-4 text-sm text-center leading-5 text-gray-900 whitespace-no-wrap">
+                        <button @click="deleteCompany(item.id)"
+                                class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                            Delete</button>
+                    </td>
                 </tr>
             </template>
             </tbody>
@@ -54,14 +60,24 @@ import { onMounted } from 'vue';
 export default {
     // This is a Vue 3 syntax to define the component setup()
     setup() {
-        // We need only two things from the useCompanies() composable
-        const { companies, getCompanies } = useCompanies()
+        // We need only 3 things from the useCompanies() composable
+        const { companies, getCompanies,destroyCompany } = useCompanies()
+
+        const deleteCompany = async (id) => {
+            if (!window.confirm('You sure?')) {
+                return
+            }
+
+            await destroyCompany(id)
+            await getCompanies()
+        }
 
         // We get the companies immediately
         onMounted(getCompanies)
 
         return {
             companies, // and return them to use in <template>
+            deleteCompany
         }
     }
 }
