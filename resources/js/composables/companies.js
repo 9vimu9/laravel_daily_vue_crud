b18ac06f7d67529bd70c5eac6abf2417e96826f7
vim <9wimu9@gmail.com>
2022-05-25 21:49:04 +0530

    resources/js/composables/companies.js

--- resources/js/composables/companies.js
+++ resources/js/composables/companies.js
@@ -58,3 +58,16 @@ export default function useCompanies() {
         updateCompany
     }
 }
+/*
+* A few things to note here.
+
+The usual standard name of the function of Composable is useSomething(), and it doesn’t necessarily need to be the same as the filename.
+
+Next, we use axios for the API requests, which is by default included in Laravel installation, we just need to import it and can make the requests like axios.get() and axios.post().
+
+Next, what is that ref thing? It is shorter for “reference”, and as it is said in the official Vue 3 documentation, “In Vue 3.0 we can make any variable reactive anywhere with a new ref function”. So, when we define our variables with, for example, const companies = ref([]) syntax, now whenever we call getCompanies, companies will be mutated and the view will be updated to reflect the change.
+
+The next thing to notice is that we use Vue Router to redirect to the list, by its name companies.index, after a successful store/update method. In case of validation errors with code 422, we parse the error structure and turn it into a String variable of errors.
+
+Finally, we define what we return from the Composable file – all variables and methods.
+* */
